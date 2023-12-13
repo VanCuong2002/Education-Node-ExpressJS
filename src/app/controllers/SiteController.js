d453b7907ba7cf5af465bab9d89ee410cd41548e
VanCuong2002 <cuongcoca12345@gmail.com>
2023-12-14 01:26:59 +0700

    [CRUD] Read from DB

--- src/app/controllers/SiteController.js
+++ src/app/controllers/SiteController.js
@@ -1,12 +1,21 @@
+const Course = require("../models/Course");
+const { mutipleMongooseToObject } = require("../util/mongoose");
+
 class SiteController {
     // [GET], /home
-    home(req, res) {
-        res.render('home');
+    home(req, res, next) {
+        Course.find({})
+            .then((courses) => {
+                res.render("home", {
+                    courses: mutipleMongooseToObject(courses),
+                });
+            })
+            .catch(next);
     }
 
     // [GET], /search
     search(req, res) {
-        res.render('search');
+        res.render("search");
     }
 }
 module.exports = new SiteController();
--- src/index.js
+++ src/index.js
@@ -1,31 +1,35 @@
-const path = require('path');
-const express = require('express');
-const morgan = require('morgan');
-const handlebars = require('express-handlebars');
+const path = require("path");
+const express = require("express");
+const morgan = require("morgan");
+const handlebars = require("express-handlebars");
 
 const app = express();
 const port = 3000;
-const route = require('./routes');
+const route = require("./routes");
+const db = require("./config/db");
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
+
+// Connecting to MongoDB
+db.connect();
 
 //HTTP logger
-app.use(morgan('combined'));
+app.use(morgan("combined"));
 
 //Template engine
 app.engine(
-    'hbs',
+    "hbs",
     handlebars({
-        extname: '.hbs',
-    }),
+        extname: ".hbs",
+    })
 );
-app.set('view engine', 'hbs');
-app.set('views', path.join(__dirname, 'resources\\views'));
+app.set("view engine", "hbs");
+app.set("views", path.join(__dirname, "resources", "views"));
 
 //route init
 route(app);
 
 // 127.0.0.1 - localhost:3000
 app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`);
+    console.log(`App listening at http://localhost:${port}`);
 });
